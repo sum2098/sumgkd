638b4833d42a42636d62b370531ff2417ce3ecd1
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-02-09 16:34:51 +0800

    Add files via upload

---
+++ src/apps/com.miui.systemAdSolution.ts
@@ -0,0 +1,14 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.miui.systemAdSolution',
+  name: '智能服务',
+  groups: [
+    {
+        key: 5,
+        name: '不希望看到',
+        activityIds: 'null',
+        rules: '@[text="不感兴趣"] + [text="减少这类内容"]','@[text="不感兴趣"] + [text="重复出现"]',
+    }
+  ],
+});
