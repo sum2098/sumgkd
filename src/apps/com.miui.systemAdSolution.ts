62d90cdc8e820a123c4d94c01dd19976797d4e5c
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-02-09 17:02:04 +0800

    Update com.miui.systemAdSolution.ts

--- src/apps/com.miui.systemAdSolution.ts
+++ src/apps/com.miui.systemAdSolution.ts
@@ -5,10 +5,16 @@ export default defineGkdApp({
   name: '智能服务',
   groups: [
     {
-      key: 5,
+      key: 1,
       name: '不希望看到',
       activityIds: 'null',
       rules: '@[text="不感兴趣"] + [text="减少这类内容"]',
+    },
+    {
+      key: 2,
+      name: '不感兴趣',
+      activityIds: 'null',
+      rules: '@[text="不感兴趣"] + [text="重复出现"]',
     },
   ],
 });
