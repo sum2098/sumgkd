ee180848800b864897142b57b185cf07f322cf3c
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-02-09 16:07:57 +0800

    Add files via upload

---
+++ src/apps/com.duokan.reader.ts
@@ -0,0 +1,14 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.duokan.reader',
+  name: '多看阅读',
+  groups: [
+    {
+        key: 3,
+        name: '全屏广告',
+        activityIds: '.DkMainActivity',
+        rules: '[text="点击卡片查看详情"]+@[text="广告"]',
+    }
+  ],
+});
