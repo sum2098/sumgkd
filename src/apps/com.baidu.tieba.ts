2474bfce0c88fc0e44bc89c8b3bcaa9c9e25a2d9
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-02-10 17:39:36 +0800

    Update com.baidu.tieba.ts

--- src/apps/com.baidu.tieba.ts
+++ src/apps/com.baidu.tieba.ts
@@ -3,5 +3,14 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.baidu.tieba',
   name: '百度贴吧',
-  groups: [],
+  groups: [
+    {
+      key: 1,
+      name: '首页直播',
+      activityIds: '.tblauncher.MainTabActivity',
+      rules: '[text="直播中"] -1 * < [name="android.widget.LinearLayout"] -2 * < [name="android.widget.RelativeLayout"] -2 * > [name="android.view.View"] +3 [name="android.widget.LinearLayout"] > * +1 @[name="android.widget.ImageView"]',
+    },
+    {
+
+    },
 });
