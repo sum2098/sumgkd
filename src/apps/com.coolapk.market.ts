a2547fb7100d0526aa4fcb2e259fa90080259752
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-02-10 16:56:10 +0800

    Update com.coolapk.market.ts

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -5,16 +5,7 @@ export default defineGkdApp({
   name: '酷安',
   groups: [
     {
-      key: 1,
-      name: '评论区广告',
-      activityIds: 'null',
-      rules: '[text="关闭"]',
-    },
-    {
-      key: 2,
-      name: '启动全屏广告',
-      activityIds: 'null',
-      rules: '[text="感谢反馈，已经为您关闭该广告"] + @[text="知道了"]',
+      
     },
   ],
 });
