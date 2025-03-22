282203c9c9cea866cd056b03e3b74fb5a2af644a
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-03-22 20:15:52 +0800

    Update com.duokan.reader.ts

--- src/apps/com.duokan.reader.ts
+++ src/apps/com.duokan.reader.ts
@@ -8,13 +8,10 @@ export default defineGkdApp({
       key: 1,
       name: '全屏广告',
       activityIds: '.DkMainActivity',
-      rules: '[text="点击卡片查看详情"] + @[text="广告"]',
+      rules: [
+         '[text="点击卡片查看详情"] + @[text="广告"]',
+         '[text="感谢反馈，已经为您关闭该广告"] + @[text="知道了"]',
+             ],
     },
-    {
-      key: 2,
-      name: '全屏广告2',
-      activityIds: '.DkMainActivity',
-      rules: '[text="感谢反馈，已经为您关闭该广告"] + @[text="知道了"]',
-    },
-  ],
+        ],
 });
