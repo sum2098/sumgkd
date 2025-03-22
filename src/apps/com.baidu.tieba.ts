39fa44e012934ffb6d8ffdb3f7e9c6bfc1b4225d
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-03-22 18:46:01 +0800

    Update com.baidu.tieba.ts

--- src/apps/com.baidu.tieba.ts
+++ src/apps/com.baidu.tieba.ts
@@ -13,9 +13,16 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '不感兴趣',
+      name: '首页直播不感兴趣',
       activityIds: '.tblauncher.MainTabActivity',
       rules: '[text="选择不喜欢的理由"] +1 @[name="android.view.View"]',
+    },
+    {
+      key: 3,
+      name: '首页广告',
+      activityIds: '.tblauncher.MainTabActivity',
+      rules: '[text="立即查看"] -1 * < [name="android.widget.RelativeLayout"] -2 * < [name="android.widget.LinearLayout"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.LinearLayout"] > [name="android.widget.FrameLayout"] +2 * > * > @[name="android.widget.FrameLayout"]',
+      '[text="选择不喜欢理由"] +1 @[name="android.view.View"]',
     },
   ],
 });
