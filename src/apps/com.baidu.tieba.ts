d86b84db20ef0cc722e7972562834fe169c0ffec
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-03-22 20:08:35 +0800

    Update com.baidu.tieba.ts

--- src/apps/com.baidu.tieba.ts
+++ src/apps/com.baidu.tieba.ts
@@ -17,6 +17,7 @@ export default defineGkdApp({
         '[text="功能"] -1 [text="隐私"] -1 [text="权限"] -3 * < [name="android.view.ViewGroup"] -1 [name="android.widget.RelativeLayout"] < * < * < [name="android.widget.FrameLayout"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.ImageView"] +3 @[name="android.widget.FrameLayout"]',
         '[name="android.view.ViewGroup"] -1 [name="android.view.ViewGroup"] < * < * < [name="android.widget.FrameLayout"] -2 * < [name="android.widget.LinearLayout"] -1 [name="android.widget.RelativeLayout"] > * > * +2 * > * > @[name="android.widget.FrameLayout"]',
         '[text="选择不喜欢理由"] +1 @[name="android.view.View"]',
+        '[name="android.widget.TextView"] < * < [name="android.widget.LinearLayout"] +1 [name="android.widget.RelativeLayout"] > * > @[name="android.widget.FrameLayout"]',
       ],
     },
     {
@@ -24,11 +25,15 @@ export default defineGkdApp({
       name: '评论区广告',
       activityIds: '.pb.pb.main.PbActivity',
       rules: [
+        '[text="直播中"] -1 * < [name="android.widget.LinearLayout"] -2 * < [name="android.widget.RelativeLayout"] -2 * > [name="android.view.View"] +3 [name="android.widget.LinearLayout"] > * +1 @[name="android.widget.ImageView"]',
+        '[text="选择不喜欢的理由"] +1 @[name="android.view.View"]',
+        '[text="立即查看"] -1 * < [name="android.widget.RelativeLayout"] -2 * < [name="android.widget.LinearLayout"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.LinearLayout"] > [name="android.widget.FrameLayout"] +2 * > * > @[name="android.widget.FrameLayout"]',
         '[text="功能"] -1 [text="隐私"] -1 [text="权限"] -3 * < [name="android.view.ViewGroup"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.RelativeLayout"] > [name="android.widget.TextView"] +1 @[name="android.widget.FrameLayout"]',
         '[text="功能"] -1 [text="隐私"] -1 [text="权限"] -3 * < [name="android.view.ViewGroup"] -1 [name="android.widget.RelativeLayout"] > * +2 @[name="android.widget.FrameLayout"]',
         '[text="功能"] -1 [text="隐私"] -1 [text="权限"] -3 * < [name="android.view.ViewGroup"] -1 [name="android.widget.RelativeLayout"] < * < * < [name="android.widget.FrameLayout"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.ImageView"] +3 @[name="android.widget.FrameLayout"]',
         '[name="android.view.ViewGroup"] -1 [name="android.view.ViewGroup"] < * < * < [name="android.widget.FrameLayout"] -2 * < [name="android.widget.LinearLayout"] -1 [name="android.widget.RelativeLayout"] > * > * +2 * > * > @[name="android.widget.FrameLayout"]',
         '[text="选择不喜欢理由"] +1 @[name="android.view.View"]',
+        '[name="android.widget.TextView"] < * < [name="android.widget.LinearLayout"] +1 [name="android.widget.RelativeLayout"] > * > @[name="android.widget.FrameLayout"]',
       ],
     },
   ],
