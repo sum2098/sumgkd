329feb2c63dad57f27ebdd22f0496cebe5b538ea
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-03-22 20:18:15 +0800

    Update com.miui.systemAdSolution.ts

--- src/apps/com.miui.systemAdSolution.ts
+++ src/apps/com.miui.systemAdSolution.ts
@@ -8,17 +8,14 @@ export default defineGkdApp({
       key: 1,
       name: '不希望看到',
       activityIds: 'null',
-      rules: '@[text="不感兴趣"] + [text="减少这类内容"]',
+      rules: [
+        '@[text="不感兴趣"] + [text="减少这类内容"]',
+        '[text="不感兴趣"] +1 * > * > * > [text="不感兴趣"]',
+      ],
     },
     {
       key: 2,
-      name: '不感兴趣',
-      activityIds: 'null',
-      rules: '[text="不感兴趣"] +1 * > * > * > [text="不感兴趣"]',
-    },
-    {
-      key: 3,
-      name: '不希望看到2',
+      name: '不希望看到(旧版)',
       activityIds: 'com.xiaomi.ad.feedback.FeedbackReasonWindow',
       rules: '@[text="不感兴趣"] +2 [text="出现过于频繁"]',
     },
