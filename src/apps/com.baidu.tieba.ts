916ca8def6f095fb4e4ea41eefa9dbda504b7447
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-03-22 20:01:32 +0800

    Update com.baidu.tieba.ts

--- src/apps/com.baidu.tieba.ts
+++ src/apps/com.baidu.tieba.ts
@@ -12,8 +12,11 @@ export default defineGkdApp({
         '[text="直播中"] -1 * < [name="android.widget.LinearLayout"] -2 * < [name="android.widget.RelativeLayout"] -2 * > [name="android.view.View"] +3 [name="android.widget.LinearLayout"] > * +1 @[name="android.widget.ImageView"]',
         '[text="选择不喜欢的理由"] +1 @[name="android.view.View"]',
         '[text="立即查看"] -1 * < [name="android.widget.RelativeLayout"] -2 * < [name="android.widget.LinearLayout"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.LinearLayout"] > [name="android.widget.FrameLayout"] +2 * > * > @[name="android.widget.FrameLayout"]',
-        '[text="选择不喜欢理由"] +1 @[name="android.view.View"]',
+        '[text="功能"] -1 [text="隐私"] -1 [text="权限"] -3 * < [name="android.view.ViewGroup"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.RelativeLayout"] > [name="android.widget.TextView"] +1 @[name="android.widget.FrameLayout"]',
+        '[text="功能"] -1 [text="隐私"] -1 [text="权限"] -3 * < [name="android.view.ViewGroup"] -1 [name="android.widget.RelativeLayout"] > * +2 @[name="android.widget.FrameLayout"]',
+        '[text="功能"] -1 [text="隐私"] -1 [text="权限"] -3 * < [name="android.view.ViewGroup"] -1 [name="android.widget.RelativeLayout"] < * < * < [name="android.widget.FrameLayout"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.ImageView"] +3 @[name="android.widget.FrameLayout"]',
         '[name="android.view.ViewGroup"] -1 [name="android.view.ViewGroup"] < * < * < [name="android.widget.FrameLayout"] -2 * < [name="android.widget.LinearLayout"] -1 [name="android.widget.RelativeLayout"] > * > * +2 * > * > @[name="android.widget.FrameLayout"]',
+        '[text="选择不喜欢理由"] +1 @[name="android.view.View"]',
       ],
     },
     {
