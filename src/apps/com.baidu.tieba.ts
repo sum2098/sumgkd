4e46ccf30a2c1c0065cef18bb0c45eabea2124d1
sum2098 <128816512+sum2098@users.noreply.github.com>
2025-03-22 18:55:58 +0800

    Update com.baidu.tieba.ts

--- src/apps/com.baidu.tieba.ts
+++ src/apps/com.baidu.tieba.ts
@@ -6,21 +6,15 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '首页直播',
+      name: '首页直播和广告',
       activityIds: '.tblauncher.MainTabActivity',
       rules: [
         '[text="直播中"] -1 * < [name="android.widget.LinearLayout"] -2 * < [name="android.widget.RelativeLayout"] -2 * > [name="android.view.View"] +3 [name="android.widget.LinearLayout"] > * +1 @[name="android.widget.ImageView"]',
         '[text="选择不喜欢的理由"] +1 @[name="android.view.View"]',
+        '[text="立即查看"] -1 * < [name="android.widget.RelativeLayout"] -2 * < [name="android.widget.LinearLayout"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.LinearLayout"] > [name="android.widget.FrameLayout"] +2 * > * > @[name="android.widget.FrameLayout"]',
+        '[text="选择不喜欢理由"] +1 @[name="android.view.View"]'
       ],
     },
-    {
-      key: 2,
-      name: '首页广告',
-      activityIds: '.tblauncher.MainTabActivity',
-      rules: [
-        '[text="立即查看"] -1 * < [name="android.widget.RelativeLayout"] -2 * < [name="android.widget.LinearLayout"] -1 [name="android.widget.RelativeLayout"] > [name="android.widget.LinearLayout"] > [name="android.widget.FrameLayout"] +2 * > * > @[name="android.widget.FrameLayout"]',
-        '[text="选择不喜欢理由"] +1 @[name="android.view.View"]',
-      ],
     },
   ],
 });
